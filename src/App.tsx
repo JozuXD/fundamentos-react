2b23b1357dce0b464747d5d09aded433ed389af4
JozuXD <souza15.josue@gmail.com>
2023-04-13 15:53:00 -0300

    inicio dos componentes home, navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
+import Navbar from './components/estaticos/navbar/Navbar'
 import Home from './paginas/home/Home'
+import Footer from './components/estaticos/footer/Footer'
+import { Grid } from "@material-ui/core"
 import './App.css'
 
 function App() {
-  
+
   return (
-    <Home />
+    <>
+      <Navbar />
+        <Home />
+      <Footer />
+
+    </>
   )
 }
 
