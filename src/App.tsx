44e9d78590a609731614b1a4482914ecc9632557
JozuXD <souza15.josue@gmail.com>
2023-04-26 01:51:39 -0300

    implementação do componente cadastro postagem

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadatroUsuario';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroTema from './components/temas/cadsatroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import CadastroPostagem from './components/postagens/cadastroPostagem/CadastroPostagem';
 
 function App() {
 
@@ -16,13 +20,32 @@ function App() {
       <Navbar />
       <div style={{ minHeight: '100vh' }}>
         <Routes> // Antigo Switch
+
           <Route path="/" element={<Login />} />
+          
+          
           <Route path="/login" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
+
           <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-          <Route path="/listapostagem" element={<ListaPostagem />} />
-          <Route path="/listatema" element={<ListaTema />} />
-       
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/posts" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPostagem />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPostagem />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
         </Routes>
       </div>
       <Footer />
